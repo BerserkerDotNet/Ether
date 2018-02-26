38c46d4ff1a715d566a9523208f625b73d68387e
Andrii Snihyr <war_chief@bigmir.net>
2018-02-26 10:14:21 -0800

    Authorize attribute for SignalR and force long polling as it does not fall back on error.

--- Ether/Hubs/LiveUpdates.cs
+++ Ether/Hubs/LiveUpdates.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Ether.Hubs
 {
+    [Authorize]
     public class LiveUpdatesHub : Hub
     {
     }
