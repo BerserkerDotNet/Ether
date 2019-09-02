13f3d0d0fda55649ba48e42214e32f129545eeb3
Andrii Snihyr <war_chief@bigmir.net>
2019-09-01 22:32:51 -0700

    Repositories page to use Redux store

---
+++ Ether/Actions/Async/DeleteRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Ether.Redux.Interfaces;
+using Ether.Types;
+using Ether.ViewModels;
+
+namespace Ether.Actions.Async
+{
+    public class DeleteRepository : IAsyncAction<VstsRepositoryViewModel>
+    {
+        private readonly EtherClient _client;
+        private readonly JsUtils _jsUtils;
+
+        public DeleteRepository(EtherClient client, JsUtils jsUtils)
+        {
+            _client = client;
+            _jsUtils = jsUtils;
+        }
+
+        public async Task Execute(IDispatcher dispatcher, VstsRepositoryViewModel repository)
+        {
+            await _client.Delete<VstsRepositoryViewModel>(repository.Id);
+            // TODO: instead of refresh delete?
+            await dispatcher.Dispatch<FetchRepositories>();
+            await _jsUtils.NotifySuccess("Delete", $"Repository {repository.Name} was deleted successfully.");
+        }
+    }
+}
---
+++ Ether/Actions/Async/SaveRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Ether.Redux.Interfaces;
+using Ether.Types;
+using Ether.ViewModels;
+
+namespace Ether.Actions.Async
+{
+    public class SaveRepository : IAsyncAction<VstsRepositoryViewModel>
+    {
+        private readonly EtherClient _client;
+
+        public SaveRepository(EtherClient client)
+        {
+            _client = client;
+        }
+
+        public async Task Execute(IDispatcher dispatcher, VstsRepositoryViewModel repository)
+        {
+            await _client.Save(repository);
+            // TODO: instead of refresh insert?
+            await dispatcher.Dispatch<FetchRepositories>();
+        }
+    }
+}
