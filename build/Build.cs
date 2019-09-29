2cb2706eae711e90443c240f4b954ebd15b43113
Andrii Snihyr <war_chief@bigmir.net>
2019-09-28 19:42:21 -0700

    artifacts directory as a parameter

--- build/Build.cs
+++ build/Build.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.Execution;
-using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
@@ -25,9 +19,10 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [Solution] readonly Solution Solution;
+    [Parameter("Path to artifacts directory - Default is /artifacts")]
+    readonly AbsolutePath ArtifactsDirectory = RootDirectory / "artifacts";
 
-    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    [Solution] readonly Solution Solution;
 
     Target Clean => _ => _
         .Before(Restore)
