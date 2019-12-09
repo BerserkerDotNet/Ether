eaabb0cc13328ffd111afc5d5c99c8c815815276
Artur Filar <v-arfila@microsoft.com>
2019-12-09 11:59:03 -0800

    reason column

---
+++ src/Ether.Api/Types/Excel/ExcelColumn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NPOI.SS.UserModel;
+
+namespace Ether.Types.Excel
+{
+    public class ExcelColumn
+    {
+        public ExcelColumn(string name, bool isDisplayed = true)
+        {
+            Name = name;
+            IsDisplayed = isDisplayed;
+        }
+
+        public string Name { get; private set; }
+
+        public bool IsDisplayed { get; set; }
+    }
+}
--- src/Ether.Api/Types/Excel/WorkItemsReportToExcelConverter.cs
+++ src/Ether.Api/Types/Excel/WorkItemsReportToExcelConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Ether.ViewModels;
 using Ether.ViewModels.Types;
 using NPOI.HSSF.Util;
@@ -11,25 +12,6 @@ namespace Ether.Types.Excel
 {
     public class WorkItemsReportToExcelConverter : ReportToExcelConverter
     {
-        private static readonly string[] HeaderColumns = new[]
-        {
-            "Workitem",
-            "Title",
-            "Type",
-            "Estimated (Days)",
-            "Time Spent (Days)",
-        };
-
-        private static readonly string[] HeaderColumnsWithTags = new[]
-        {
-            "Workitem",
-            "Title",
-            "Type",
-            "Tags",
-            "Estimated (Days)",
-            "Time Spent (Days)",
-        };
-
         public override byte[] Convert(ReportViewModel report)
         {
             if (report == null)
@@ -46,16 +28,49 @@ namespace Ether.Types.Excel
             var memory = new MemoryStream();
             {
                 var workbook = new XSSFWorkbook();
-                CreateReportSheet(workbook, "Resolved Work Items", prReport.ResolvedWorkItems, prReport);
-                CreateReportSheet(workbook, "Work Items In Pull Request", prReport.WorkItemsInReview, prReport);
-                CreateReportSheet(workbook, "Active Work Items", prReport.ActiveWorkItems, prReport, true);
+                CreateResolvedWorkItemsReport(workbook, "Resolved Work Items", prReport.ResolvedWorkItems, prReport);
+                CreateWorkItemsInPullRequestReport(workbook, "Work Items In Pull Request", prReport.WorkItemsInReview, prReport);
+                CreateActiveWorkItemsReport(workbook, "Active Work Items", prReport.ActiveWorkItems, prReport);
 
                 workbook.Write(memory);
                 return memory.ToArray();
             }
         }
 
-        private void CreateReportSheet(XSSFWorkbook workbook, string sectionName, IEnumerable<WorkItemDetail> workItems, WorkItemsReportViewModel prReport, bool includeTags = false)
+        private ExcelColumn[] GetHeaderColumns() => new[]
+        {
+            new ExcelColumn("Workitem"),
+            new ExcelColumn("Title"),
+            new ExcelColumn("Type"),
+            new ExcelColumn("Tags", false),
+            new ExcelColumn("Reason", false),
+            new ExcelColumn("Estimated (Days)"),
+            new ExcelColumn("Time Spent (Days)")
+        };
+
+        private bool ShouldInclude(ExcelColumn[] columns, string columnName) => columns.Single(col => col.Name == columnName).IsDisplayed;
+
+        private void CreateResolvedWorkItemsReport(XSSFWorkbook workbook, string sectionName, IEnumerable<WorkItemDetail> workItems, WorkItemsReportViewModel prReport)
+        {
+            var headerColumns = GetHeaderColumns();
+            headerColumns.Single(x => x.Name == "Reason").IsDisplayed = true;
+            CreateReportSheet(workbook, sectionName, workItems, prReport, headerColumns);
+        }
+
+        private void CreateWorkItemsInPullRequestReport(XSSFWorkbook workbook, string sectionName, IEnumerable<WorkItemDetail> workItems, WorkItemsReportViewModel prReport)
+        {
+            var headerColumns = GetHeaderColumns();
+            CreateReportSheet(workbook, sectionName, workItems, prReport, headerColumns);
+        }
+
+        private void CreateActiveWorkItemsReport(XSSFWorkbook workbook, string sectionName, IEnumerable<WorkItemDetail> workItems, WorkItemsReportViewModel prReport)
+        {
+            var headerColumns = GetHeaderColumns();
+            headerColumns.Single(x => x.Name == "Tags").IsDisplayed = true;
+            CreateReportSheet(workbook, sectionName, workItems, prReport, headerColumns);
+        }
+
+        private void CreateReportSheet(XSSFWorkbook workbook, string sectionName, IEnumerable<WorkItemDetail> workItems, WorkItemsReportViewModel prReport, ExcelColumn[] columns)
         {
             var excelSheet = workbook.CreateSheet(sectionName);
             var creationHelper = workbook.GetCreationHelper();
@@ -65,14 +80,15 @@ namespace Ether.Types.Excel
             hlinkfont.Underline = FontUnderlineType.Single;
             hlinkfont.Color = HSSFColor.Blue.Index;
             hlinkstyle.SetFont(hlinkfont);
+            var includeTags = ShouldInclude(columns, "Tags");
+            var includeReason = ShouldInclude(columns, "Reason");
 
-            SetHeader(excelSheet, includeTags);
+            SetHeader(excelSheet, columns);
             int rowIdx = 1, cellIdx;
             foreach (var reportEntry in workItems)
             {
                 cellIdx = 0;
                 var row = excelSheet.CreateRow(rowIdx);
-
                 var idCell = row.CreateCell(cellIdx++, CellType.String);
                 idCell.SetCellValue(reportEntry.WorkItemId);
                 var link = creationHelper.CreateHyperlink(HyperlinkType.Url);
@@ -84,6 +100,11 @@ namespace Ether.Types.Excel
                 if (includeTags)
                 {
                     row.CreateCell(cellIdx++, CellType.String).SetCellValue(reportEntry.Tags);
+                }
+
+                if (includeReason)
+                {
+                    row.CreateCell(cellIdx++, CellType.String).SetCellValue(reportEntry.Reason);
                 }
 
                 row.CreateCell(cellIdx++, CellType.Numeric).SetCellValue(reportEntry.EstimatedToComplete);
@@ -101,13 +122,18 @@ namespace Ether.Types.Excel
                 _ = summaryRow.CreateCell(cellIdx++, CellType.String);
             }
 
+            if (includeReason)
+            {
+                _ = summaryRow.CreateCell(cellIdx++, CellType.String);
+            }
+
             summaryRow.CreateCell(cellIdx++, CellType.Numeric).SetCellValue(prReport.GetTotalEstimated(workItems));
             summaryRow.CreateCell(cellIdx, CellType.Numeric).SetCellValue(prReport.GetTotalTimeSpent(workItems));
 
             // AutosizeCells(excelSheet, summaryRow.Cells.Count);
         }
 
-        private void SetHeader(ISheet sheet, bool includeTags)
+        private void SetHeader(ISheet sheet, ExcelColumn[] columns)
         {
             var boldFont = sheet.Workbook.CreateFont();
             boldFont.Boldweight = (short)FontBoldWeight.Bold;
@@ -116,11 +142,10 @@ namespace Ether.Types.Excel
 
             var row = sheet.CreateRow(0);
             int idx = 0;
-            var columns = includeTags ? HeaderColumnsWithTags : HeaderColumns;
-            foreach (var column in columns)
+            foreach (var column in columns.Where(col => col.IsDisplayed))
             {
                 var cell = row.CreateCell(idx, CellType.String);
-                cell.SetCellValue(column);
+                cell.SetCellValue(column.Name);
                 cell.CellStyle = boldStyle;
                 idx++;
             }
